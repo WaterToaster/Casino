44bd89164590744b034ebf099788d08ce6c04620
Wahoo-Jones-Neumont <80439396+Wahoo-Jones-Neumont@users.noreply.github.com>
2022-02-08 19:42:38 -0700

    Update BJ.js

--- BlackJack/BJ.js
+++ BlackJack/BJ.js
@@ -1,7 +1,8 @@
 
 
-let ThreexDeck = [
+var ThreexDeck = [
     HAce = [
+        type = "Ace",
         count = 3,
         img = "",
         value = [ 
@@ -10,6 +11,7 @@ let ThreexDeck = [
         isAce = true
     ],
     DAce = [
+        type = "Ace",
         count = 3,
         img = "",
         value = [ 
@@ -18,6 +20,7 @@ let ThreexDeck = [
         isAce = true
     ],
     SAce = [
+        type = "Ace",
         count = 3,
         img = "",
         value = [ 
@@ -26,6 +29,7 @@ let ThreexDeck = [
         isAce = true
     ],
     CAce = [
+        type = "Ace",
         count = 3,
         img = "",
         value = [ 
@@ -35,72 +39,84 @@ let ThreexDeck = [
     ],
     //Hearts
     HTwo = [
+        type = "Two",
         count = 3,
         img = "",
         value = 2,
         isAce = false
     ],
     HThree = [
+        type = "Three",
         count = 3,
         img = "",
         value = 3,
         isAce = false
     ],
     HFour = [
+        type = "Four",
         count = 3,
         img = "",
         value = 4,
         isAce = false
     ],
     HFive = [
+        type = "Five",
         count = 3,
         img = "",
         value = 5,
         isAce = false
     ],
     HSix = [
+        type = "Six",
         count = 3,
         img = "",
         value = 6,
         isAce = false
     ],
     HSeven = [
+        type = "Seven",
         count = 3,
         img = "",
         value = 7,
         isAce = false
     ],
     HEight = [
+        type = "Eight",
         count = 3,
         img = "",
         value = 8,
         isAce = false
     ],
     HNine = [
+        type = "Nine",
         count = 3,
         img = "",
         value = 9,
         isAce = false
     ],
     HTen = [
+        type = "Ten",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     HJack = [
+        type = "Jack",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     HQueen = [
+        type = "Queen",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     HKing = [
+        type = "King",
         count = 3,
         img = "",
         value = 10,
@@ -108,72 +124,84 @@ let ThreexDeck = [
     ],
     //Diamonds
     DTwo = [
+        type = "Two",
         count = 3,
         img = "",
         value = 2,
         isAce = false
     ],
     DThree = [
+        type = "Three",
         count = 3,
         img = "",
         value = 3,
         isAce = false
     ],
     DFour = [
+        type = "Four",
         count = 3,
         img = "",
         value = 4,
         isAce = false
     ],
     DFive = [
+        type = "Five",
         count = 3,
         img = "",
         value = 5,
         isAce = false
     ],
     DSix = [
+        type = "Six",
         count = 3,
         img = "",
         value = 6,
         isAce = false
     ],
     DSeven = [
+        type = "Seven",
         count = 3,
         img = "",
         value = 7,
         isAce = false
     ],
     DEight = [
+        type = "Eight",
         count = 3,
         img = "",
         value = 8,
         isAce = false
     ],
     DNine = [
+        type = "Nine",
         count = 3,
         img = "",
         value = 9,
         isAce = false
     ],
     DTen = [
+        type = "Ten",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     DJack = [
+        type = "Jack",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     DQueen = [
+        type = "Queen",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     DKing = [
+        type = "King",
         count = 3,
         img = "",
         value = 10,
@@ -181,72 +209,84 @@ let ThreexDeck = [
     ],
     //Spades
     STwo = [
+        type = "Two",
         count = 3,
         img = "",
         value = 2,
         isAce = false
     ],
     SThree = [
+        type = "Three",
         count = 3,
         img = "",
         value = 3,
         isAce = false
     ],
     SFour = [
+        type = "Four",
         count = 3,
         img = "",
         value = 4,
         isAce = false
     ],
     SFive = [
+        type = "Five",
         count = 3,
         img = "",
         value = 5,
         isAce = false
     ],
     SSix = [
+        type = "Six",
         count = 3,
         img = "",
         value = 6,
         isAce = false
     ],
     SSeven = [
+        type = "Seven",
         count = 3,
         img = "",
         value = 7,
         isAce = false
     ],
     SEight = [
+        type = "Eight",
         count = 3,
         img = "",
         value = 8,
         isAce = false
     ],
     SNine = [
+        type = "Nine",
         count = 3,
         img = "",
         value = 9,
         isAce = false
     ],
     STen = [
+        type = "Ten",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     SJack = [
+        type = "Jack",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     SQueen = [
+        type = "Queen",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     SKing = [
+        type = "King",
         count = 3,
         img = "",
         value = 10,
@@ -254,75 +294,98 @@ let ThreexDeck = [
     ],
     //Clubs
     CTwo = [
+        type = "Two",
         count = 3,
         img = "",
         value = 2,
         isAce = false
     ],
     CThree = [
+        type = "Three",
         count = 3,
         img = "",
         value = 3,
         isAce = false
     ],
     CFour = [
+        type = "Four",
         count = 3,
         img = "",
         value = 4,
         isAce = false
     ],
     CFive = [
+        type = "Five",
         count = 3,
         img = "",
         value = 5,
         isAce = false
     ],
     CSix = [
+        type = "Six",
         count = 3,
         img = "",
         value = 6,
         isAce = false
     ],
     CSeven = [
+        type = "Seven",
         count = 3,
         img = "",
         value = 7,
         isAce = false
     ],
     CEight = [
+        type = "Eight",
         count = 3,
         img = "",
         value = 8,
         isAce = false
     ],
     CNine = [
+        type = "Nine",
         count = 3,
         img = "",
         value = 9,
         isAce = false
     ],
     CTen = [
+        type = "Ten",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     CJack = [
+        type = "Jack",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     CQueen = [
+        type = "Queen",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ],
     CKing = [
+        type = "King",
         count = 3,
         img = "",
         value = 10,
         isAce = false
     ]
 ];
+
+var HouseHand = [];
+var PlayerHand1 = [];
+var PlayerHand2 = [];
+
+const AllowSplit = (PH =>{
+    if(PH[0].type = PH[1].type){
+
+    }
+});
+
