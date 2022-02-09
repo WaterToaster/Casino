6a3a55dc5a43ec56e2df844369e9e95162747953
Wahoo-Jones-Neumont <80439396+Wahoo-Jones-Neumont@users.noreply.github.com>
2022-02-09 09:30:23 -0700

    Update BJ.js

--- BlackJack/BJ.js
+++ BlackJack/BJ.js
@@ -4,7 +4,7 @@ var ThreexDeck = [
     HAce = [
         type = "Ace",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHeartsA.png",
         value = [ 
             lowVal = 1,
             HighVal = 11],
@@ -13,7 +13,7 @@ var ThreexDeck = [
     DAce = [
         type = "Ace",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamondA.png",
         value = [ 
             lowVal = 1,
             HighVal = 11],
@@ -22,7 +22,7 @@ var ThreexDeck = [
     SAce = [
         type = "Ace",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpadesA.png",
         value = [ 
             lowVal = 1,
             HighVal = 11],
@@ -31,7 +31,7 @@ var ThreexDeck = [
     CAce = [
         type = "Ace",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubsA.png",
         value = [ 
             lowVal = 1,
             HighVal = 11],
@@ -41,84 +41,84 @@ var ThreexDeck = [
     HTwo = [
         type = "Two",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts2.png",
         value = 2,
         isAce = false
     ],
     HThree = [
         type = "Three",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts3.png",
         value = 3,
         isAce = false
     ],
     HFour = [
         type = "Four",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts4.png",
         value = 4,
         isAce = false
     ],
     HFive = [
         type = "Five",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts5.png",
         value = 5,
         isAce = false
     ],
     HSix = [
         type = "Six",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts6.png",
         value = 6,
         isAce = false
     ],
     HSeven = [
         type = "Seven",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts7.png",
         value = 7,
         isAce = false
     ],
     HEight = [
         type = "Eight",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts8.png",
         value = 8,
         isAce = false
     ],
     HNine = [
         type = "Nine",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts9.png",
         value = 9,
         isAce = false
     ],
     HTen = [
         type = "Ten",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHearts10.png",
         value = 10,
         isAce = false
     ],
     HJack = [
         type = "Jack",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHeartsJ.png",
         value = 10,
         isAce = false
     ],
     HQueen = [
         type = "Queen",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHeartsQ.png",
         value = 10,
         isAce = false
     ],
     HKing = [
         type = "King",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardHeartsK.png",
         value = 10,
         isAce = false
     ],
@@ -126,84 +126,84 @@ var ThreexDeck = [
     DTwo = [
         type = "Two",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond2.png",
         value = 2,
         isAce = false
     ],
     DThree = [
         type = "Three",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond3.png",
         value = 3,
         isAce = false
     ],
     DFour = [
         type = "Four",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond4.png",
         value = 4,
         isAce = false
     ],
     DFive = [
         type = "Five",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond5.png",
         value = 5,
         isAce = false
     ],
     DSix = [
         type = "Six",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond6.png",
         value = 6,
         isAce = false
     ],
     DSeven = [
         type = "Seven",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond7.png",
         value = 7,
         isAce = false
     ],
     DEight = [
         type = "Eight",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond8.png",
         value = 8,
         isAce = false
     ],
     DNine = [
         type = "Nine",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond9.png",
         value = 9,
         isAce = false
     ],
     DTen = [
         type = "Ten",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamond10.png",
         value = 10,
         isAce = false
     ],
     DJack = [
         type = "Jack",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamondJ.png",
         value = 10,
         isAce = false
     ],
     DQueen = [
         type = "Queen",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamondQ.png",
         value = 10,
         isAce = false
     ],
     DKing = [
         type = "King",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardDiamondK.png",
         value = 10,
         isAce = false
     ],
@@ -211,84 +211,84 @@ var ThreexDeck = [
     STwo = [
         type = "Two",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades2.png",
         value = 2,
         isAce = false
     ],
     SThree = [
         type = "Three",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades3.png",
         value = 3,
         isAce = false
     ],
     SFour = [
         type = "Four",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades4.png",
         value = 4,
         isAce = false
     ],
     SFive = [
         type = "Five",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades5.png",
         value = 5,
         isAce = false
     ],
     SSix = [
         type = "Six",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades6.png",
         value = 6,
         isAce = false
     ],
     SSeven = [
         type = "Seven",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades7.png",
         value = 7,
         isAce = false
     ],
     SEight = [
         type = "Eight",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades8.png",
         value = 8,
         isAce = false
     ],
     SNine = [
         type = "Nine",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades9.png",
         value = 9,
         isAce = false
     ],
     STen = [
         type = "Ten",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpades10.png",
         value = 10,
         isAce = false
     ],
     SJack = [
         type = "Jack",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpadesJ.png",
         value = 10,
         isAce = false
     ],
     SQueen = [
         type = "Queen",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpadesQ.png",
         value = 10,
         isAce = false
     ],
     SKing = [
         type = "King",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardSpadesK.png",
         value = 10,
         isAce = false
     ],
@@ -296,84 +296,84 @@ var ThreexDeck = [
     CTwo = [
         type = "Two",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs2.png",
         value = 2,
         isAce = false
     ],
     CThree = [
         type = "Three",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs3.png",
         value = 3,
         isAce = false
     ],
     CFour = [
         type = "Four",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs4.png",
         value = 4,
         isAce = false
     ],
     CFive = [
         type = "Five",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs5.png",
         value = 5,
         isAce = false
     ],
     CSix = [
         type = "Six",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs6.png",
         value = 6,
         isAce = false
     ],
     CSeven = [
         type = "Seven",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs7.png",
         value = 7,
         isAce = false
     ],
     CEight = [
         type = "Eight",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs8.png",
         value = 8,
         isAce = false
     ],
     CNine = [
         type = "Nine",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs9.png",
         value = 9,
         isAce = false
     ],
     CTen = [
         type = "Ten",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubs10.png",
         value = 10,
         isAce = false
     ],
     CJack = [
         type = "Jack",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubsJ.png",
         value = 10,
         isAce = false
     ],
     CQueen = [
         type = "Queen",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubsQ.png",
         value = 10,
         isAce = false
     ],
     CKing = [
         type = "King",
         count = 3,
-        img = "",
+        img = "BlackJack\Assets\Cards\cardClubsK.png",
         value = 10,
         isAce = false
     ]
