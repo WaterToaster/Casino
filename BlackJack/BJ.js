dc846056794f5fd6f586d828f66551aa31a52040
Wahoo-Jones-Neumont <80439396+Wahoo-Jones-Neumont@users.noreply.github.com>
2022-02-08 19:41:34 -0700

    Update BJ.js
    Added a 3xDeck and the building blocks to allow split

---
+++ BlackJack/BJ.js
@@ -0,0 +1,328 @@
+
+
+let ThreexDeck = [
+    HAce = [
+        count = 3,
+        img = "",
+        value = [ 
+            lowVal = 1,
+            HighVal = 11],
+        isAce = true
+    ],
+    DAce = [
+        count = 3,
+        img = "",
+        value = [ 
+            lowVal = 1,
+            HighVal = 11],
+        isAce = true
+    ],
+    SAce = [
+        count = 3,
+        img = "",
+        value = [ 
+            lowVal = 1,
+            HighVal = 11],
+        isAce = true
+    ],
+    CAce = [
+        count = 3,
+        img = "",
+        value = [ 
+            lowVal = 1,
+            HighVal = 11],
+        isAce = true
+    ],
+    //Hearts
+    HTwo = [
+        count = 3,
+        img = "",
+        value = 2,
+        isAce = false
+    ],
+    HThree = [
+        count = 3,
+        img = "",
+        value = 3,
+        isAce = false
+    ],
+    HFour = [
+        count = 3,
+        img = "",
+        value = 4,
+        isAce = false
+    ],
+    HFive = [
+        count = 3,
+        img = "",
+        value = 5,
+        isAce = false
+    ],
+    HSix = [
+        count = 3,
+        img = "",
+        value = 6,
+        isAce = false
+    ],
+    HSeven = [
+        count = 3,
+        img = "",
+        value = 7,
+        isAce = false
+    ],
+    HEight = [
+        count = 3,
+        img = "",
+        value = 8,
+        isAce = false
+    ],
+    HNine = [
+        count = 3,
+        img = "",
+        value = 9,
+        isAce = false
+    ],
+    HTen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    HJack = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    HQueen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    HKing = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    //Diamonds
+    DTwo = [
+        count = 3,
+        img = "",
+        value = 2,
+        isAce = false
+    ],
+    DThree = [
+        count = 3,
+        img = "",
+        value = 3,
+        isAce = false
+    ],
+    DFour = [
+        count = 3,
+        img = "",
+        value = 4,
+        isAce = false
+    ],
+    DFive = [
+        count = 3,
+        img = "",
+        value = 5,
+        isAce = false
+    ],
+    DSix = [
+        count = 3,
+        img = "",
+        value = 6,
+        isAce = false
+    ],
+    DSeven = [
+        count = 3,
+        img = "",
+        value = 7,
+        isAce = false
+    ],
+    DEight = [
+        count = 3,
+        img = "",
+        value = 8,
+        isAce = false
+    ],
+    DNine = [
+        count = 3,
+        img = "",
+        value = 9,
+        isAce = false
+    ],
+    DTen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    DJack = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    DQueen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    DKing = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    //Spades
+    STwo = [
+        count = 3,
+        img = "",
+        value = 2,
+        isAce = false
+    ],
+    SThree = [
+        count = 3,
+        img = "",
+        value = 3,
+        isAce = false
+    ],
+    SFour = [
+        count = 3,
+        img = "",
+        value = 4,
+        isAce = false
+    ],
+    SFive = [
+        count = 3,
+        img = "",
+        value = 5,
+        isAce = false
+    ],
+    SSix = [
+        count = 3,
+        img = "",
+        value = 6,
+        isAce = false
+    ],
+    SSeven = [
+        count = 3,
+        img = "",
+        value = 7,
+        isAce = false
+    ],
+    SEight = [
+        count = 3,
+        img = "",
+        value = 8,
+        isAce = false
+    ],
+    SNine = [
+        count = 3,
+        img = "",
+        value = 9,
+        isAce = false
+    ],
+    STen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    SJack = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    SQueen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    SKing = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    //Clubs
+    CTwo = [
+        count = 3,
+        img = "",
+        value = 2,
+        isAce = false
+    ],
+    CThree = [
+        count = 3,
+        img = "",
+        value = 3,
+        isAce = false
+    ],
+    CFour = [
+        count = 3,
+        img = "",
+        value = 4,
+        isAce = false
+    ],
+    CFive = [
+        count = 3,
+        img = "",
+        value = 5,
+        isAce = false
+    ],
+    CSix = [
+        count = 3,
+        img = "",
+        value = 6,
+        isAce = false
+    ],
+    CSeven = [
+        count = 3,
+        img = "",
+        value = 7,
+        isAce = false
+    ],
+    CEight = [
+        count = 3,
+        img = "",
+        value = 8,
+        isAce = false
+    ],
+    CNine = [
+        count = 3,
+        img = "",
+        value = 9,
+        isAce = false
+    ],
+    CTen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    CJack = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    CQueen = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ],
+    CKing = [
+        count = 3,
+        img = "",
+        value = 10,
+        isAce = false
+    ]
+];
